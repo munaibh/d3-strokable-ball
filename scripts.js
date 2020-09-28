d0c6df9055882e8fcb7f59cd4d2f48f8146ea142
munaibh <munaibh@gmail.com>
2020-09-28 17:16:13 +0100

    Upgrading to d3 v6 and cleaning up events.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const svg = d3.select('.globe-container')
+const files = ['https://raw.githubusercontent.com/d3/d3.github.com/master/world-110m.v1.json', 'destinations.json']
 const width = window.innerWidth, height = window.innerHeight
 const scale = width > height ? (height/2) : (width/2)
 const projection = d3.geoOrthographic()
@@ -10,18 +11,17 @@ const projection = d3.geoOrthographic()
 const initialScale = projection.scale()
 const path = d3.geoPath().projection(projection)  
 const sensitivity = 75
+const promises = files.map((url) => d3.json(url))
 
 svg.call(d3.drag().on('drag', handleGlobeDrag))
 svg.call(d3.zoom().on('zoom', onGlobeZoom))
 
-queue()
-  .defer(d3.json, "https://raw.githubusercontent.com/d3/d3.github.com/master/world-110m.v1.json")
-  .defer(d3.json, "destinations.json")
-  .await(function(error, world, places) {
-    drawGlobeToDom(world, places)
-    drawDataToDom(world, places)
-    resolvePathPositions()
-  })
+Promise.all(promises).then((values) =>{
+  const [world, places] = values
+  drawGlobeToDom(world, places)
+  drawDataToDom(world, places)
+  resolvePathPositions()
+})
 
 function drawGlobeToDom(world, places) {
   svg
@@ -100,25 +100,26 @@ function resolvePathPositions() {
     })
 }
 
-function handleGlobeDrag() {
+function handleGlobeDrag(event) {
+  event.sourceEvent.stopPropagation()
   const rotate = projection.rotate()
   const k = sensitivity / projection.scale()
-  projection.rotate([rotate[0] + d3.event.dx * k, rotate[1] - d3.event.dy * k])
+  projection.rotate([rotate[0] + event.dx * k, rotate[1] - event.dy * k])
   svg.selectAll(".globe__land").attr("d", path)
   resolvePathPositions()
 }
 
-function onGlobeZoom() {
-  const isScaled = d3.event.transform.k <= 0.3
-  if(isScaled) return d3.event.transform.k = 0.3
-  projection.scale(initialScale * d3.event.transform.k)
+function onGlobeZoom(event) {
+  const isScaled = event.transform.k <= 0.3
+  if(isScaled) return event.transform.k = 0.3
+  projection.scale(initialScale * event.transform.k)
   svg.selectAll(".globe__land").attr("d", path)
   svg.selectAll(".globe__base").attr("r", projection.scale())
   resolvePathPositions()
 }
 
-function handlePointClicked({properties}) {
-  if(!window.useLinks) d3.event.preventDefault()
+function handlePointClicked(event, {properties}) {
+  if(!window.useLinks) event.preventDefault()
   d3.select('.globe-properties')
     .html(`<pre>${JSON.stringify(properties, undefined, 2)}</span>`)
 }
